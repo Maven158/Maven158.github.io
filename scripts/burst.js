d604631e625ce876c25645ffbf1cd42c38ec435f
Maven158 <95103800+Maven158@users.noreply.github.com>
2023-04-21 23:52:32 -0500

    Starburst

--- scripts/burst.js
+++ scripts/burst.js
@@ -1,43 +1,62 @@
 export function loadCanvas() {
-  const canvas = document.createElement('canvas');
-  const element = document.getElementsByClassName('burstBox');
-  $(element).append(canvas);
-  $(canvas).addClass('burst');
-  const ctx = canvas.getContext('2d');
-  const size = [4, 8, 12, 16, 20, 24, 28, 32];
-  const colors = ['#fd79a8', '#e84393', '#ffeaa7', '#fdcb6e', '#55efc4', '#00b894', '#6c5ce7', '#a29bfe'];
-  const particles = Array.from({ length: 500 }, initializeParticle);
-  // base_image = new Image();
-  // base_image.src = 'images/banner.png';
-  // base_image.onload = function(){
-  //   // ctx.drawImage(base_image, 0, 0, canvas.width, canvas.height);
-  // }
+  if (document.getElementsByClassName('burst').length == 0) {
+    var canvas = document.createElement('canvas');
+    var element = document.getElementsByClassName('burstBox');
+    var image = document.getElementsByClassName('banner');
+    $(element).append(canvas);
+    $(canvas).addClass('burst');
+    console.log($(image).width());
+    console.log($(image).height());
+    canvas.width = $(image).width() + 24;
+	  canvas.height = $(image).height() + 24;
+    var ctx = canvas.getContext('2d');
+    var size = [8, 12, 16, 20, 24, 28, 32, 36];
+    var colors = ['#fd79a8', '#e84393', '#ffeaa7', '#fdcb6e', '#55efc4', '#00b894', '#6c5ce7', '#a29bfe'];
+    var particles = Array.from({ length: 500 }, initializeParticle);
+    var doAnim = true;
+  } else {
+    $(canvas).remove();
+    particles = [];
+    ctx = null;
+    doAnim = false;
+  }
 
   function initializeParticle() {
     return {
-      x: (Math.random() * canvas.width/200) + canvas.width/2,
-      y: canvas.height/2,
-      dx: Math.random() * 3 - 0,
-      dy: Math.random() * 3 - 0,
+      x: canvas.width/6.3,
+      y: canvas.height/1.8,
+      dx: (Math.random() * 4 - 1) + 1,
+      dy: (Math.random() * 4 - 1) + 1,
       dirx: Math.random() * 2 - 1,
       diry: Math.random() * 2 - 1,
       fillStyle: colors[Math.floor(Math.random() * 8)],
       font: size[Math.floor(Math.random() * 8)] + 'px bold sans-serif'
     }
   }
-// context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
   function update(time) {
-    particles.forEach(updateParticle);
-    draw(time);
-    requestAnimationFrame(update);
+    if (particles.length == 0 && document.getElementsByClassName('burst').length != 0) {
+      $(canvas).remove();
+      doAnim = false;
+      ctx = null;
+      loadCanvas();
+    }
+    if (particles.length == 0 && document.getElementsByClassName('burst').length == 0) {
+
+      doAnim = false;
+      ctx = null;
+    }
+    if (doAnim) {
+      particles.forEach(updateParticle);
+      draw(time);
+      requestAnimationFrame(update);
+    }
   }
 
   function updateParticle(particle) {
-    // if(particle.y < 0 || particle.x < 0 || particle.x > canvas.width || particle.y > canvas.height) {
-    //   particle.x = (Math.random() * canvas.width/200) + canvas.width/2;
-    //   particle.y = canvas.height/2;
-    // } 
-
+    if ((particle.y < 0 || particle.y > canvas.height) || (particle.x > canvas.width || particle.x < 0)) {
+      particles.splice(particles.indexOf(particle), 1);
+    } 
     if (particle.dirx > 0) {
       particle.x = particle.x + particle.dx;
       if (particle.diry > 0) {
